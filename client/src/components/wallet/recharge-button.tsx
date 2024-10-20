1958ddf619a1bb5217d22a25e793a15d15b45cab
subhash131 <subhashnayak131@gmail.com>
2024-10-20 17:28:00 +0530

    add update wallet balance

--- client/src/components/wallet/recharge-button.tsx
+++ client/src/components/wallet/recharge-button.tsx
@@ -1,11 +1,13 @@
 "use client";
+import { setBalance } from "@/state-manager/features/wallet-balance";
 import { RootState } from "@/state-manager/store";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toast } from "sonner";
 
 const RechargeButton = ({ amount }: { amount: number | undefined }) => {
   const { username } = useSelector((state: RootState) => state.username);
+  const dispatch = useDispatch();
   const recharge = async () => {
     if (!username) {
       toast.error("Username not found!");
@@ -27,6 +29,17 @@ const RechargeButton = ({ amount }: { amount: number | undefined }) => {
       },
     });
     const data = await res.json();
+
+    if (res.status == 200) {
+      const data = await fetch(
+        `http://localhost:8000/balance/inr/${username}`
+      ).then(async (res) => await res.json());
+
+      if (data && data.balance) {
+        dispatch(setBalance(data.balance));
+      }
+    }
+
     toast.info(data.msg);
   };
   return (
