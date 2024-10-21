3da10c57f73982e39b983e3b91e5c487f980d78a
subhash131 <subhashnayak131@gmail.com>
2024-10-22 02:45:03 +0530

    create a helper function: fetchMarket

---
+++ server/src/utils/fetch-market.ts
@@ -0,0 +1,37 @@
+import { STOCK_BALANCES } from "../db";
+
+export type Market = {
+  [name: string]: {
+    [stockType: string]: number;
+  };
+};
+export function fetchMarket() {
+  const market: Market = {};
+  for (const user of Object.keys(STOCK_BALANCES)) {
+    const symbol = STOCK_BALANCES[user];
+    for (const sym of Object.keys(symbol)) {
+      const stock = symbol[sym];
+      Object.keys(stock).forEach((current) => {
+        const availableStocks = Math.abs(
+          stock[current].quantity - stock[current].locked
+        );
+        if (!market[sym]) {
+          market[sym] = { [current]: availableStocks };
+        } else {
+          if (market[sym][current]) {
+            market[sym] = {
+              ...market[sym],
+              [current]: availableStocks + market[sym][current],
+            };
+          } else {
+            market[sym] = {
+              ...market[sym],
+              [current]: availableStocks,
+            };
+          }
+        }
+      });
+    }
+  }
+  return market;
+}
