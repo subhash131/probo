851078fcce7308bf4821b913ad5cbd02908dfb34
subhash131 <subhashnayak131@gmail.com>
2024-10-21 11:27:09 +0530

    add market state

--- client/src/state-manager/store.ts
+++ client/src/state-manager/store.ts
@@ -1,8 +1,9 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import walletBalance from "./features/wallet-balance";
 import username from "./features/username";
+import market from "./features/market";
 
-const reducer = combineReducers({ walletBalance, username });
+const reducer = combineReducers({ walletBalance, username, market });
 
 export const store = configureStore({
   reducer,
