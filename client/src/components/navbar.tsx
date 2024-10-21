d8900c8698def8f29387eba6140df6de8c75f441
subhash131 <subhashnayak131@gmail.com>
2024-10-21 11:28:29 +0530

    add new routes to navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import WalletBalance from "./wallet-balance";
 import Image from "next/image";
 import Username from "./username";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -13,6 +14,14 @@ const Navbar = () => {
         height={1}
         className="w-fit h-14"
       />
+      <div className="w-full flex gap-2 items-center justify-center">
+        <Link href="/wallet" className="px-3 py-1 hover:bg-gray-200 rounded-md">
+          Wallet
+        </Link>
+        <Link href="/market" className="px-3 py-1 hover:bg-gray-200 rounded-md">
+          Market
+        </Link>
+      </div>
       <div className="flex gap-3 items-center justify-center text-center">
         <Username />
         <WalletBalance />
